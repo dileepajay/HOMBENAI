cfbe4b48e4ddeaaaad167282425ed5836510cd64
dileepajay <me.dreamwalker@gmail.com>
2024-07-18 14:43:37 +0530

    init push

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+#t2
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
